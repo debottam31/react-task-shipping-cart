16c81a36ec98df3f106faf36fa288fd0c821ae2e
auriga <sumit.pritmani@amnic.com>
2023-09-28 22:19:15 +0530

    added: comments

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -24,9 +24,13 @@ type CartProviderProps = {
 export const CartProvider = ({ children }: CartProviderProps) => {
   const [items, setItems] = useState<CartItem[]>([]);
 
-  const addItem = (newItem: MenuItem | CartItem) => {
+  /**
+   * Function to handle add item to cart
+   * @param newItem item from menu or cart
+   */
+  const addItem = (newItem: MenuItem | CartItem): void => {
     const alreadyAddedItem = items.find((item) => item.name === newItem.name);
-
+    // increment the quantity of the cart item if one is already present, else add the new item
     if (alreadyAddedItem) {
       const updateItems = items.map((item) => {
         if (item.name === newItem.name)
@@ -39,29 +43,42 @@ export const CartProvider = ({ children }: CartProviderProps) => {
     }
   };
 
+  /**
+   * Function to handle checkout
+   */
   const submit = () => {
     console.log("submitting order");
     window.alert("Order Placed!");
     setItems([]);
   };
 
-  const handleDecreaseItemQuantity = (itemName: MenuItem["name"]) => {
+  /**
+   * Handle reduce item quantity in the cart
+   * @param itemName name of the item, quantity of which to reduce
+   */
+  const handleDecreaseItemQuantity = (itemName: MenuItem["name"]): void => {
     const decreasedItem = items.find(
       (item) => item.name === itemName
     ) as CartItem;
     let updatedCartItems = [];
 
-    if (decreasedItem.quantity === 1)
+    // if item quantity is 1, remove it from cart, else reduce the quantity by 1
+    if (decreasedItem.quantity === 1) {
       updatedCartItems = items.filter((item) => item !== decreasedItem);
-    else
+    } else {
       updatedCartItems = items.map((item) =>
         item === decreasedItem ? { ...item, quantity: item.quantity - 1 } : item
       );
+    }
 
     setItems(updatedCartItems);
   };
 
-  const handleIncreaseItemQuantity = (itemName: MenuItem["name"]) => {
+  /**
+   * Handle increment item quantity in the cart
+   * @param itemName name of the item, quantity of which to reduce
+   */
+  const handleIncreaseItemQuantity = (itemName: MenuItem["name"]): void => {
     const newItem = items.find((item) => item.name === itemName) as CartItem;
     addItem(newItem);
   };
